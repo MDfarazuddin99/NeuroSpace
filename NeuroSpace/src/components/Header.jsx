fbe5ab70b168c3cecb959328f5cbb385cf2f70bf
MDfarazuddin99 <52372209+MDfarazuddin99@users.noreply.github.com>
2024-01-07 09:40:37 +0530

    Navbar responsive v3

--- NeuroSpace/src/components/Header.jsx
+++ NeuroSpace/src/components/Header.jsx
@@ -4,11 +4,11 @@ import {Link} from 'react-router-dom';
 function Header(){
     return (
         <Flex bg="teal.500" color="white"  align="center" p={1} direction={{base: 'column', md: 'row'}} justify={'center'}>
-          <Text fontSize={{base: 'md', md:'2xl'}} fontWeight={'bold'} ml={{base: 0, md: "5%"}} mt={{base: 2}}>
+          <Text fontSize={{base: 'md', md:'2xl'}} fontWeight={'bold'} ml={{base: 0, md: "5%"}} mt={{base: 1, md: 0}}>
             TIMINGS: MON-SAT: 8AM – 12PM  |   5PM – 9PM
           </Text>
           <Spacer display={{base: 'none', md: 'block'}}/>
-          <Button bg={'orange'} rightIcon={<ArrowForwardIcon />} flex={"None"} mr={{md: "10%"}} _hover={{bg:'blue.50'}} mb={{base: 2}}>
+          <Button bg={'orange'} rightIcon={<ArrowForwardIcon />} flex={"None"} mr={{md: "10%"}} _hover={{bg:'blue.50'}} mb={{base: 1, md : 0}}>
           <Link to="/book-appointment">BOOK APPOINTMENT</Link>
           </Button>
         </Flex>

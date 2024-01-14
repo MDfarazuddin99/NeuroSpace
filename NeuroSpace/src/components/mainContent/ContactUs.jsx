0269dbc26f6475b75d01bf536ffd0856e8f329ed
MDfarazuddin99 <52372209+MDfarazuddin99@users.noreply.github.com>
2024-01-14 14:13:37 +0530

    minor change

--- NeuroSpace/src/components/mainContent/ContactUs.jsx
+++ NeuroSpace/src/components/mainContent/ContactUs.jsx
@@ -1,12 +1,28 @@
-import {Container, Flex} from "@chakra-ui/react";
+import {Container, Flex, Heading, Text, HStack} from "@chakra-ui/react";
 
+import {CalendarIcon, TimeIcon,PhoneIcon} from "@chakra-ui/icons"
+import { FaLocationDot } from "react-icons/fa6";
 
 
 const ContactUs = () => {
   return (
     <Container variant={'main'} py={"2vh"} >
-        <Flex minH="1000px" bg="blue.50" w="75%" mx="12.5%">
-
+        <Flex bg="blue.50" w="75%" mx="12.5%">
+          <Flex bg="green" w="100%" flexDirection={'row'}>
+            <Heading>
+              Contact Us Anytime, 7 days a Week
+            </Heading>
+            <Flex flexDir={'column'}>
+              <Text>Contact Info</Text>
+              <HStack><FaLocationDot/><Text> Vinayak Nagar, Hydershakote, Bandlaguda Jagir, Telangana 500091</Text></HStack>
+              <HStack><PhoneIcon/><Text>09618994555</Text></HStack>
+            </Flex>
+            <Flex flexDir={'column'}>
+              <Text>Open Hours</Text>
+              <HStack><CalendarIcon/><Text>Monday - Sunday</Text></HStack>
+              <HStack><TimeIcon/><Text>8am-12pm & 5pm-9pm</Text></HStack>
+            </Flex>
+          </Flex>
         </Flex>
     </Container>
   )
--- NeuroSpace/src/components/mainContent/Home.jsx
+++ NeuroSpace/src/components/mainContent/Home.jsx
@@ -4,56 +4,74 @@ import hplogo from "../../assets/homepagelogo.png";
 import shakeeb from "../../assets/shakeeb.jpeg";
 import bgimage from "../../assets/bg_image.jpg";
 
+import c1 from "../../assets/clinic/clinic1.jpg";
+import c2 from "../../assets/clinic/clinic2.jpg";
+import c3 from "../../assets/clinic/clinic3.jpg";
+import c4 from "../../assets/clinic/clinic4.jpg";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
 const Home = () => {
   return (
-    <Flex flexDirection={'column'}>
+    <Flex flexDirection={"column"}>
       <Box
-          backgroundImage={bgimage}
-          minH= {{base: '20vh', md: '40vh'}}
-          backgroundSize= "cover"
-          backgroundPosition= "center"
-          backgroundRepeat= "no-repeat"
+        backgroundImage={bgimage}
+        minH={{ base: "20vh", md: "40vh" }}
+        backgroundSize="cover"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+      ></Box>
+      <Flex minH="1000px" bg="blue.100" w="full" flexDir={"column"}>
+        <Box
+          display={{ md: "none", base: "flex" }}
+          bg="blue.200"
+          justifyContent={"center"}
         >
-  
+          <Heading fontSize={"xl"} color="blue.600">
+            NeuroSpace
+          </Heading>
         </Box>
         <Flex
-          minH="1000px"
-          bg="blue.50"
-          w="full"
-          flexDir={"column"}
+          px={"2%"}
+          py={"1%"}
+          bg="blue.100"
+          w="100%"
+          h={{ md: 325, base: 105 }}
         >
-          <Box display={{ md: "none", base: "flex" }} bg="blue.200" justifyContent={'center'}>
-            <Heading fontSize={"xl"} color="blue.600" >
-              NeuroSpace
-            </Heading>
-          </Box>
-          <Flex
-            px={"2%"}
-            py={"1%"}
-            bg="blue.100"
-            w="100%"
-            h={{ md: 325, base: 105 }}
-          >
-            <Image src={hplogo} h={{ md: 300, base: 100 }} />
-            <Flex alignItems={"center"} justifyContent={"center"} w="100%">
-              <Box display={{ base: "none", md: "initial" }}>
-                <Heading fontSize={{ md: "9xl", base: "3xl" }} color="blue.600">
-                  NeuroSpace
-                </Heading>
-                <Heading fontSize={{ md: "2xl", base: "md" }}>
-                  Excellence in Brain, Spine and Pain Care
-                </Heading>
-              </Box>
-            </Flex>
-            <Image
-              src={shakeeb}
-              h={{ md: 300, base: 100 }}
-              borderRadius={"50%"}
-              bg="blue.300"
-              p={3}
-            />
+          <Image src={hplogo} h={{ md: 300, base: 100 }} />
+          <Flex alignItems={"center"} justifyContent={"center"} w="100%">
+            <Box display={{ base: "none", md: "initial" }}>
+              <Heading fontSize={{ md: "9xl", base: "3xl" }} color="blue.600">
+                NeuroSpace
+              </Heading>
+              <Heading fontSize={{ md: "2xl", base: "md" }}>
+                Excellence in Brain, Spine and Pain Care
+              </Heading>
+            </Box>
           </Flex>
-        </Flex>      
+          <Image
+            src={shakeeb}
+            h={{ md: 300, base: 100 }}
+            borderRadius={"50%"}
+            bg="blue.300"
+            p={3}
+          />
+        </Flex>
+        <Flex w={{base:"95%", md:"90%"}} mx={{base:"2.5%", md:"5%"}}>
+          <Carousel centerMode="True" autoPlay="True" infiniteLoop="True" showArrows="True">
+            <Box>
+              <Image src={c1} h={"50vh"} />
+            </Box>
+            <Box>
+              <Image src={c2} h={"50vh"} />
+            </Box>
+            <Box>
+              <Image src={c3} h={"50vh"} />
+            </Box>
+          </Carousel>
+        </Flex>
+      </Flex>
     </Flex>
   );
 };

1e26e7191e595c4da5401a9edd1c164af6472d2e
MDfarazuddin99 <52372209+MDfarazuddin99@users.noreply.github.com>
2024-01-24 15:18:09 +0530

    Elfsight Reviews added/ testing2

--- NeuroSpace/src/components/mainContent/Reviews.jsx
+++ NeuroSpace/src/components/mainContent/Reviews.jsx
@@ -1,31 +1,31 @@
 import { Container, Flex } from "@chakra-ui/react";
-// import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import { useEffect } from "react";
 
 const Reviews = () => {
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = 'https://static.elfsight.com/platform/platform.js';
-        script.defer = true;
-    
-        document.head.appendChild(script);
-    
-        return () => {
-          document.head.removeChild(script);
-        };
-      }, []);
-    
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://static.elfsight.com/platform/platform.js";
+    script.defer = true;
+
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
   return (
     <Container variant="main">
       <Flex backdropFilter="blur(5px)" h="auto" justifyContent={"center"}>
         <Flex bg="blue.50" p={"2%"} my="2%">
-          {/* <Helmet defer={false}> */}
+          <Helmet defer={false}>
             <script
               src="https://static.elfsight.com/platform/platform.js"
               data-use-service-core
               defer
             ></script>
-          {/* </Helmet> */}
+          </Helmet>
           <div
             className="elfsight-app-2c429c63-3fbd-4c09-9ed3-fbb24fd7f9b4"
             data-elfsight-app-lazy
@@ -33,6 +33,24 @@ const Reviews = () => {
         </Flex>
       </Flex>
     </Container>
+
+    // <Container variant="main">
+    //   <Flex backdropFilter="blur(5px)" h="auto" justifyContent={"center"}>
+    //     <Flex bg="blue.50" p={"2%"} my="2%">
+    //       {/* <Helmet defer={false}> */}
+    // <script
+    //   src="https://static.elfsight.com/platform/platform.js"
+    //   data-use-service-core
+    //   defer
+    // ></script>
+    //       {/* </Helmet> */}
+    //   <div
+    //     className="elfsight-app-2c429c63-3fbd-4c09-9ed3-fbb24fd7f9b4"
+    //     data-elfsight-app-lazy
+    //   ></div>{" "}
+    //     </Flex>
+    //   </Flex>
+    // </Container>
   );
 };
 
